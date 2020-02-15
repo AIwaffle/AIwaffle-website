e8e1a3ccd7615d8ea4af50c569d7e73f1e69bd6d
Kevin Li <kevinli020508@gmail.com>
2020-02-15 06:53:37 -0500

    Fix routing between pages

--- src/Main.elm
+++ src/Main.elm
@@ -3,11 +3,12 @@ module Main exposing (main)
 import Browser
 import Browser.Navigation as Nav
 import Html
-import Url
 import Page.Home as Home
 import Page.NotFound as NotFound
 import Page.Tutorial as Tutorial
-import Url.Parser as Parser exposing (Parser, (</>))
+import Url
+import Url.Parser as Parser exposing ((</>), Parser)
+
 
 
 -- MAIN
@@ -43,10 +44,7 @@ type Page
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-  route url
-    { key = key
-    , page = Home <| Tuple.first <| Home.init ()
-    }
+  route url { key = key, page = NotFound }
 
 
 
@@ -67,7 +65,7 @@ update message model =
     LinkClicked urlRequest ->
       case urlRequest of
         Browser.Internal url ->
-          ( model, Nav.pushUrl model.key (Url.toString url) )
+          route url model |> reloadIfNeeded url model
 
         Browser.External href ->
           ( model, Nav.load href )
@@ -77,47 +75,84 @@ update message model =
 
     HomeMsg msg ->
       case model.page of
-        Home home -> stepHome model (Home.update msg home)
-        _             -> ( model, Cmd.none )
+        Home home ->
+          stepHome model (Home.update msg home)
+
+        _ ->
+          ( model, Cmd.none )
 
     TutorialMsg msg ->
       case model.page of
-        Tutorial tutorial -> stepTutorial model (Tutorial.update msg tutorial)
-        _             -> ( model, Cmd.none )
+        Tutorial tutorial ->
+          stepTutorial model (Tutorial.update msg tutorial)
+
+        _ ->
+          ( model, Cmd.none )
 
     NotFoundMsg _ ->
       ( model, Cmd.none )
 
 
-route : Url.Url -> Model -> (Model, Cmd Msg)
-route url model =
-   let
+
+-- ROUTING
+
+
+type Route
+  = HomeRoute
+  | TutorialRoute String
+  | UnknownRoute
+
+
+type Region
+  = Light
+  | Heavy
+
+
+pageToRegion : Page -> Region
+pageToRegion page =
+  case page of
+    Tutorial _ ->
+      Heavy
+
+    _ ->
+      Light
+
+
+urlToRoute : Url.Url -> Route
+urlToRoute url =
+  let
     parser =
       Parser.oneOf
-        [ Parser.map
-          ( stepHome model (Home.init ())
-          )
-          (Parser.s "home")
-        , Parser.map
-          (\tutorialName ->
-            ( model
-            , Nav.load <| Url.toString url
-            )
-          )
-          (Parser.s "tutorial" </> tutorialName_)
-        , Parser.map
-            (stepHome model (Home.init ()))
-            Parser.top
+        [ Parser.map HomeRoute Parser.top
+        , Parser.map HomeRoute (Parser.s "home")
+        , Parser.map TutorialRoute (Parser.s "tutorial" </> tutorialName_)
         ]
   in
-  case Parser.parse parser url of
-    Just answer ->
-      answer
+  Parser.parse parser url
+    |> Maybe.withDefault UnknownRoute
 
-    Nothing ->
-      ( { model | page = NotFound }
-      , Cmd.none
-      )
+
+route : Url.Url -> Model -> ( Model, Cmd Msg )
+route url model =
+  case urlToRoute url of
+    HomeRoute ->
+      stepHome model (Home.init ())
+
+    TutorialRoute content ->
+      stepTutorial model (Tutorial.init content)
+
+    UnknownRoute ->
+      ( { model | page = NotFound }, Cmd.none )
+
+
+reloadIfNeeded : Url.Url -> Model -> ( Model, Cmd msg ) -> ( Model, Cmd msg )
+reloadIfNeeded url oldModel ( newModel, cmd ) =
+  if pageToRegion newModel.page == pageToRegion oldModel.page then
+    ( newModel, Cmd.batch [ cmd, Nav.pushUrl newModel.key (Url.toString url) ] )
+
+  else
+    -- Ignore the cmd because the page will be reloaded
+    ( newModel, Nav.load (Url.toString url) )
 
 
 tutorialName_ : Parser (String -> a) a
@@ -126,17 +161,18 @@ tutorialName_ =
 
 
 stepHome : Model -> ( Home.Model, Cmd Home.Msg ) -> ( Model, Cmd Msg )
-stepHome model (home, cmds) =
+stepHome model ( home, cmds ) =
   ( { model | page = Home home }
   , Cmd.map HomeMsg cmds
   )
 
 
 stepTutorial : Model -> ( Tutorial.Model, Cmd Tutorial.Msg ) -> ( Model, Cmd Msg )
-stepTutorial model (tutorial, cmds) =
+stepTutorial model ( tutorial, cmds ) =
   ( { model | page = Tutorial tutorial }
   , Cmd.map TutorialMsg cmds
   )
+
 
 
 -- SUBSCRIPTIONS
@@ -160,16 +196,18 @@ view model =
         [ Html.map NotFoundMsg <| NotFound.view {}
         ]
       }
+
     Home home ->
       { title = "AIWaffle"
       , body =
         [ Html.map HomeMsg <| Home.view home
         ]
       }
+
     Tutorial tutorial ->
       { title = Tutorial.getContentName tutorial.contentIndex
       , body =
         [ Html.map TutorialMsg <| Tutorial.view tutorial
         ]
       }
-      
+
